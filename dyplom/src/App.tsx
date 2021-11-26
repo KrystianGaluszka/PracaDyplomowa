91920b5538b8df16bf0fa3fddaec6287716809c6
KrystianGaluszka <divero1998@gmail.com>
2021-11-26 13:46:12 +0100

    react typescript project

---
+++ dyplom/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Account } from "./components/account";
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <div className="account">
+        <Account />
+      </div>
+    </div>
+  )
+}
+
+export default App;
